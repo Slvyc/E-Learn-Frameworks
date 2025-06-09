1b9bdde1fc29ab6eb923b084a35bf4af7875079f
Slvyc <daffaalfharizy265@gmail.com>
2025-06-09 20:21:33 +0700

    gap

--- resources/views/framework.blade.php
+++ resources/views/framework.blade.php
@@ -18,7 +18,8 @@
 
     <!-- Main Container -->
     <main class="relative z-20 bg-[#141414]">
-        <div class="max-w-screen-xl px-6 top-0 flex relative gap-8">
+        <div class="px-6 top-0 flex relative gap-8">
+            
 
             <!-- Sidebar -->
             <aside class="w-1/5 relative py-8 px-4 sticky top-[80px] self-start h-[calc(100vh-80px)] mt-10">
